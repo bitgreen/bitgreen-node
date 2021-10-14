721ac88936b9f6788dbca897243d8ffad2b9e2d0
Ayush <>
2021-10-14 18:54:16 +0530

    Fixed bonds tests compilation and added test-case

--- pallets/bonds/src/tests.rs
+++ pallets/bonds/src/tests.rs
@@ -1,23 +1,24 @@
 use crate::{Error, mock::*};
 use frame_support::{assert_ok, assert_noop};
+use frame_support::error::BadOrigin;
+
+// #[test]
+// fn it_works_for_default_value() {
+// 	new_test_ext().execute_with(|| {
+// 		// Dispatch a signed extrinsic.
+// 		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
+// 		// Read pallet storage and assert an expected result.
+// 		assert_eq!(TemplateModule::something(), Some(42));
+// 	});
+// }
 
 #[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
-}
-
-#[test]
-fn correct_error_for_none_value() {
+fn create_change_settings_does_not_work_for_non_root() {
 	new_test_ext().execute_with(|| {
 		// Ensure the expected error is thrown when no value is present.
 		assert_noop!(
-			TemplateModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
+			Bonds::create_change_settings(Origin::signed(1), b"kyc".to_vec(), b"[{'document':'Profit&Loss Previous year'}]".to_vec()),
+			BadOrigin
 		);
 	});
 }
