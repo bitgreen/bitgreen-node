f65441954c361af86bf4235027119f4babe27d6d
patedetomates <93400769+patedetomates@users.noreply.github.com>
2022-03-24 07:45:13 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// Bitgreen block crawler
+// This program will listen for new blocks and store 
+// them in a local Postgresql database. 
+
+// import required dependancies
+const { processBlock, initApi} = require("./src/methods")
+
+require('dotenv').config()
+
+// main function (must be async)
+async function main () {
+    const api = await initApi()
+
+    // Retrieve the chain & node information via rpc calls
+    const [chain, nodeName, nodeVersion] = await Promise.all([
+        api.rpc.system.chain(),
+        api.rpc.system.name(),
+        api.rpc.system.version()
+    ]);
+
+    // log message to console 
+    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+
+    // We only display a couple, then unsubscribe
+    let count = 0;
+
+    // Subscribe to the new headers on-chain. The callback is fired when new headers
+    // are found, the call itself returns a promise with a subscription that can be
+    // used to unsubscribe from the newHead subscription
+    const unsubscribe = await api.rpc.chain.subscribeNewHeads(async(header) => {
+        await processBlock(api, header.number)
+
+        if (++count === 20) {
+            // unsubscribe();
+            // process.exit(0);
+        }
+    });
+}
+
+main().catch(console.error);
