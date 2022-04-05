75bafe8bce0ad805ea841b437b425bc4531422c9
patedetomates <93400769+patedetomates@users.noreply.github.com>
2022-04-04 22:54:51 -0400

    Add files via upload

---
+++ cache-engine/migrations/1649029724415_vcu-assets-generating-destroyed.js
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('vcu_assets_generating_destroyed', {
+        id: 'id',
+        block_number: {
+            type: 'int',
+            notNull: true
+        },
+        hash: {
+            type: 'varchar(66)',
+            unique: true,
+            notNull: true
+        },
+        agv_account: {
+            type: 'varchar(64)',
+            notNull: true
+        },
+        agv_id: {
+            type: 'int',
+            notNull: true
+        },
+        signer: {
+            type: 'varchar(64)',
+            notNull: true
+        },
+        date: {
+            type: 'timestamp',
+            notNull: true
+        },
+        created_at: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp')
+        }
+    })
+};
+
+exports.down = pgm => {
+    pgm.dropTable('vcu_assets_generating_destroyed')
+};
---
+++ cache-engine/migrations/1649030509677_vcu-authorized-accounts-destroyed.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('vcu_authorized_accounts_destroyed', {
+        id: 'id',
+        block_number: {
+            type: 'int',
+            notNull: true
+        },
+        hash: {
+            type: 'varchar(66)',
+            unique: true,
+            notNull: true
+        },
+        account: {
+            type: 'varchar(64)',
+            notNull: true
+        },
+        signer: {
+            type: 'varchar(64)',
+            notNull: true
+        },
+        date: {
+            type: 'timestamp',
+            notNull: true
+        },
+        created_at: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp')
+        }
+    })
+};
+
+exports.down = pgm => {
+    pgm.dropTable('vcu_authorized_accounts_destroyed')
+};
