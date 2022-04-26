2b75554d95295da6dc3703bab175dcff967e4f8d
Abhath Labs <99468748+abhath-labs@users.noreply.github.com>
2022-04-26 20:11:00 +0400

    fix para id

--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 rm -rf /tmp/parachain/alice
 
-./target/release/bitg-parachain build-spec --chain dev --raw --disable-default-bootnode > rococo-local-parachain-2000-raw.json
+./target/release/bitg-parachain build-spec --chain rococo --raw --disable-default-bootnode > rococo-bitgreen-3029-raw.json
 
-./target/release/bitg-parachain export-genesis-wasm --chain rococo-local-parachain-2000-raw.json > para-2000-wasm
+./target/release/bitg-parachain export-genesis-wasm --chain rococo-bitgreen-3029-raw.json > rococo-bitgreen-3029-wasm
 
-./target/release/bitg-parachain export-genesis-state --chain rococo-local-parachain-2000-raw.json > para-2000-genesis
+./target/release/bitg-parachain export-genesis-state --chain rococo-bitgreen-3029-raw.json > rococo-bitgreen-3029-genesis
 
 ./target/release/bitg-parachain \
 --alice \
 --collator \
 --force-authoring \
---chain rococo-local-parachain-2000-raw.json \
+--chain rococo-bitgreen-3029-raw.json \
 --base-path /tmp/parachain/alice \
 --port 40333 \
---ws-port 8844 \
+--ws-port 9944 \
+--rpc-port 9979 --rpc-cors all --discover-local \
+--rpc-external --rpc-methods=unsafe \
 -- \
 --execution wasm \
---chain ../polkadot/rococo-local-cfde.json \
---port 30343 \
---ws-port 9977
+--chain rococo.json
