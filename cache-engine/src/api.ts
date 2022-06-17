4e39c0a183784d3ba03c551d28adf9bda19e461a
rappix <rappix95@gmail.com>
2022-06-17 02:23:54 +0200

    cache engine - add cors support

--- cache-engine/src/api.ts
+++ cache-engine/src/api.ts
@@ -15,6 +15,7 @@ const port = process.env.API_PORT || 3000
 const prisma = new PrismaClient()
 
 // array of all allowed origins
+// TODO: Add list of origins
 const allowed_origins = ['*']
 const cors_options: CorsOptions = {
 	origin: allowed_origins
@@ -28,7 +29,7 @@ const mainLoop = async () => {
 	app.use(express.urlencoded({extended: true}));
 	app.use(express.json());
 
-	app.use(cors(cors_options));
+	app.use(cors());
 
 	app.get('/', function (req: Request, res: Response) {
 		res.send('Hello from BitGreen!');
