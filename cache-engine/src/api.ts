dc53ff35b782a9bf0a6f601d2e86c4c40503db15
needs <624097+needs@users.noreply.github.com>
2022-07-27 15:45:29 +0200

    Lint all TypeScript files

--- cache-engine/src/api.ts
+++ cache-engine/src/api.ts
@@ -1,24 +1,24 @@
 // Bitgreen API Server
 
 /* import packages */
-import express, { Express, Request, Response } from 'express'
-import cors, { CorsOptions } from 'cors'
-import { PrismaClient } from '@prisma/client'
-import * as dotenv from 'dotenv'
-import { initApi } from "./methods"
-import { BlockHash } from "@polkadot/types/interfaces"
+import express, { Express, Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
+import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+import { initApi } from "./methods";
+import { BlockHash } from "@polkadot/types/interfaces";
 
 /* config */
 dotenv.config();
-const port = process.env.API_PORT || 3000
+const port = process.env.API_PORT || 3000;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // array of all allowed origins
 // TODO: Add list of origins
-const allowed_origins = ['*']
+const allowed_origins = ["*"];
 const cors_options: CorsOptions = {
-	origin: allowed_origins
+	origin: allowed_origins,
 };
 
 // main function
@@ -26,47 +26,47 @@ const mainLoop = async () => {
 	/* setup app */
 	const app: Express = express();
 
-	app.use(express.urlencoded({extended: true}));
+	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 
 	app.use(cors());
 
-	app.get('/', function (req: Request, res: Response) {
-		res.send('Hello from BitGreen!');
+	app.get("/", function (req: Request, res: Response) {
+		res.send("Hello from BitGreen!");
 	});
 
-	app.get('/get-block', async(req: Request, res: Response) => {
-		let block_number = req.query.block_number as string
+	app.get("/get-block", async (req: Request, res: Response) => {
+		let block_number = req.query.block_number as string;
 
-		const api = await initApi()
+		const api = await initApi();
 
-		const block_hash = await api.rpc.chain.getBlockHash(block_number) as BlockHash
+		const block_hash = (await api.rpc.chain.getBlockHash(block_number)) as BlockHash;
 
 		let [signed_block, block_events] = await Promise.all([
 			api.rpc.chain.getBlock(block_hash),
-			api.query.system.events.at(block_hash)
+			api.query.system.events.at(block_hash),
 		]);
 
 		res.json({
 			signed_block: signed_block.toHuman(),
 			block_events: block_events.toHuman(),
-		})
-	})
+		});
+	});
 
 	// app.get('/analyze-data', db.getAnalyzeData)
 
-	app.get('/vcu/projects', async(req: Request, res: Response) => {
-		const { originator = undefined } = req.query
+	app.get("/vcu/projects", async (req: Request, res: Response) => {
+		const { originator = undefined } = req.query;
 
-		const or = originator ? {
-			OR: [
-				{ originator: originator as string }
-			]
-		} : {};
+		const or = originator
+			? {
+					OR: [{ originator: originator as string }],
+			  }
+			: {};
 
 		const vcu_projects = await prisma.vcu_projects.findMany({
 			where: {
-				...or
+				...or,
 			},
 			select: {
 				id: true,
@@ -91,62 +91,61 @@ const mainLoop = async () => {
 						end_date: true,
 						total_supply: true,
 						minted: true,
-						retired: true
-					}
+						retired: true,
+					},
 				},
 				documents: {
 					select: {
-						url: true
-					}
+						url: true,
+					},
 				},
 				images: {
 					select: {
-						url: true
-					}
+						url: true,
+					},
 				},
 				locations: {
 					select: {
 						latitude: true,
-						longitude: true
-					}
+						longitude: true,
+					},
 				},
 				royalties: {
 					select: {
 						account: true,
-						fee_percent: true
-					}
+						fee_percent: true,
+					},
 				},
 				sdgs: {
 					select: {
 						type: true,
 						description: true,
-						references: true
-					}
+						references: true,
+					},
 				},
 				videos: {
 					select: {
-						url: true
-					}
+						url: true,
+					},
 				},
-			}
-		})
+			},
+		});
 
 		res.json({
-			projects: vcu_projects
-		})
-	})
+			projects: vcu_projects,
+		});
+	});
 
-	app.get('/transactions', async(req: Request, res: Response) => {
-		const { account, date_start = '2000-01-01', date_end = '2200-01-01' } = req.query
+	app.get("/transactions", async (req: Request, res: Response) => {
+		const { account, date_start = "2000-01-01", date_end = "2200-01-01" } = req.query;
 
-		const account_query = account ? {
-			OR: [
-				{ sender: account as string },
-				{ recipient: account as string },
-			]
-		} : {};
+		const account_query = account
+			? {
+					OR: [{ sender: account as string }, { recipient: account as string }],
+			  }
+			: {};
 
-		let transactions
+		let transactions;
 		try {
 			transactions = await prisma.transactions.findMany({
 				where: {
@@ -154,7 +153,7 @@ const mainLoop = async () => {
 						gte: new Date(date_start as string),
 						lte: new Date(date_end as string),
 					},
-					...account_query
+					...account_query,
 				},
 				select: {
 					block_number: true,
@@ -162,27 +161,27 @@ const mainLoop = async () => {
 					sender: true,
 					recipient: true,
 					amount: true,
-					created_at: true
+					created_at: true,
 				},
 				orderBy: {
-					created_at: 'desc',
-				}
-			})
+					created_at: "desc",
+				},
+			});
 		} catch (e) {
-			transactions = null
+			transactions = null;
 		}
 
 		res.json({
-			transactions: transactions
-		})
-	})
+			transactions: transactions,
+		});
+	});
 
-	app.get('/transaction', async(req: Request, res: Response) => {
-		const { hash = '' } = req.query
+	app.get("/transaction", async (req: Request, res: Response) => {
+		const { hash = "" } = req.query;
 
 		const transaction = await prisma.transactions.findUnique({
 			where: {
-				hash: hash as string
+				hash: hash as string,
 			},
 			select: {
 				block_number: true,
@@ -190,120 +189,125 @@ const mainLoop = async () => {
 				sender: true,
 				recipient: true,
 				amount: true,
-				created_at: true
-			}
-		})
+				created_at: true,
+			},
+		});
 
 		res.json({
-			transaction: transaction
-		})
-	})
+			transaction: transaction,
+		});
+	});
 
-	app.get('/assets', async(req: Request, res: Response) => {
-		const assets = await prisma.assets.findMany()
+	app.get("/assets", async (req: Request, res: Response) => {
+		const assets = await prisma.assets.findMany();
 
 		res.json({
-			assets: assets
-		})
-	})
+			assets: assets,
+		});
+	});
 
-	app.get('/asset', async(req: Request, res: Response) => {
-		const { asset_id, project_id } = req.query
+	app.get("/asset", async (req: Request, res: Response) => {
+		const { asset_id, project_id } = req.query;
 
 		let asset: any = {};
 
-		if(asset_id) {
+		if (asset_id) {
 			asset = await prisma.assets.findUnique({
 				where: {
-					id: Number(asset_id)
-				}
-			})
-		} else if(project_id) {
+					id: Number(asset_id),
+				},
+			});
+		} else if (project_id) {
 			let project = await prisma.vcu_projects.findUnique({
 				where: {
-					id: Number(project_id)
+					id: Number(project_id),
 				},
 				select: {
-					asset: true
+					asset: true,
 				},
-			})
+			});
 
-			asset = project?.asset
+			asset = project?.asset;
 		}
 
 		res.json({
-			asset: asset
-		})
-	})
+			asset: asset,
+		});
+	});
 
-	app.get('/assets/transactions', async(req: Request, res: Response) => {
-		let { account = undefined, date_start = '2000-01-01', date_end = '2200-01-01', asset_id = undefined, project_id = undefined } = req.query
+	app.get("/assets/transactions", async (req: Request, res: Response) => {
+		let {
+			account = undefined,
+			date_start = "2000-01-01",
+			date_end = "2200-01-01",
+			asset_id = undefined,
+			project_id = undefined,
+		} = req.query;
 
-		const account_query = account ? {
-			OR: [
-				{ sender: account as string },
-				{ recipient: account as string },
-			],
-		} : {};
+		const account_query = account
+			? {
+					OR: [{ sender: account as string }, { recipient: account as string }],
+			  }
+			: {};
 
 		// filter by project only if asset_id is not present
-		if(!asset_id && project_id) {
+		if (!asset_id && project_id) {
 			let project = await prisma.vcu_projects.findUnique({
 				where: {
-					id: Number(project_id)
+					id: Number(project_id),
 				},
 				select: {
-					asset: true
+					asset: true,
 				},
-			})
+			});
 
 			// @ts-ignore
-			asset_id = project?.asset?.id || asset_id
+			asset_id = project?.asset?.id || asset_id;
 		}
 
-		const asset_query = asset_id ? {
-			AND: [
-				{ asset_id: Number(asset_id) }
-			]
-		} : {};
+		const asset_query = asset_id
+			? {
+					AND: [{ asset_id: Number(asset_id) }],
+			  }
+			: {};
 
-		let transactions
+		let transactions;
 		try {
 			transactions = await prisma.asset_transactions.findMany({
 				where: {
 					created_at: {
-						gte: new Date(date_start as string) ,
+						gte: new Date(date_start as string),
 						lte: new Date(date_end as string),
 					},
 					...account_query,
-					...asset_query
+					...asset_query,
 				},
 				orderBy: {
-					created_at: 'desc',
-				}
-			})
+					created_at: "desc",
+				},
+			});
 		} catch (e) {
-			transactions = null
+			transactions = null;
 		}
 
 		res.json({
-			transactions: transactions
-		})
-	})
+			transactions: transactions,
+		});
+	});
 
-	app.get('/assets/transaction', async(req: Request, res: Response) => {
-		const { hash = '' } = req.query
+	app.get("/assets/transaction", async (req: Request, res: Response) => {
+		const { hash = "" } = req.query;
 
 		const transaction = await prisma.asset_transactions.findUnique({
 			where: {
-				hash: hash as string
-			}
-		})
+				hash: hash as string,
+			},
+		});
 
 		res.json({
-			transaction: transaction
-		})
-	})
+			transaction: transaction,
+		});
+	});
 
 	// app.get('/impact_actions', db.getImpactActions)
 	// app.get('/impact_actions/auditors', db.getImpactActionsAuditors)
@@ -317,9 +321,9 @@ const mainLoop = async () => {
 
 	/* serve api */
 	const server = app.listen(port, function () {
-		console.log(`API server is listening at: http://localhost:${port}.`)
+		console.log(`API server is listening at: http://localhost:${port}.`);
 	});
-}
+};
 
 // run main function
-mainLoop().catch(console.error)
+mainLoop().catch(console.error);
