ffa0ace5fe0a78999dca4037a8a6e5424db1d1c1
Anant <anant.asty@gmail.com>
2022-09-20 09:04:23 -0600

    Cicd ready to deploy

---
+++ scripts/bob.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./root/polkadot/polkadot --chain ~/rococo-local-cfde.json --bob --tmp
---
+++ scripts/parachain.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/root/bitg-parachain/target/release/bitg-parachain --dev --alice --validator -- --chain /root/rococo-local-cfde.json
