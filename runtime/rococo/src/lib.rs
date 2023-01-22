fc58cac1744e055adf3cf01fc6482dffaf7e6915
Abhath Labs <99468748+abhath-labs@users.noreply.github.com>
2023-01-22 15:47:18 +0400

    bump version

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -174,7 +174,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bitgreen-rococo"),
 	impl_name: create_runtime_str!("bitgreen-rococo"),
 	authoring_version: 1,
-	spec_version: 1001, // v1.0.1
+	spec_version: 1002, // v1.0.2
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
