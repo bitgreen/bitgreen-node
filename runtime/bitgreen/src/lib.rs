4b647e49cf99861408dacfccdcb5f78db815c7a1
Abhath Labs <99468748+abhath-labs@users.noreply.github.com>
2023-02-20 13:14:28 +0530

    fix runtime version

--- runtime/bitgreen/src/lib.rs
+++ runtime/bitgreen/src/lib.rs
@@ -175,7 +175,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bitgreen-parachain"),
 	impl_name: create_runtime_str!("bitgreen-parachain"),
 	authoring_version: 1,
-	spec_version: 1102, // v1.1.2
+	spec_version: 1100, // v1.1.0
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -174,7 +174,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bitgreen-rococo"),
 	impl_name: create_runtime_str!("bitgreen-rococo"),
 	authoring_version: 1,
-	spec_version: 1102, // v1.1.2
+	spec_version: 1100, // v1.1.0
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
