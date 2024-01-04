56c7b53fc7e01dd9649211bc7d87ffdc78c5dd3a
Abhath Labs <99468748+abhath-labs@users.noreply.github.com>
2024-01-04 08:57:08 +0000

    update testnet artifacts

--- scripts/generate-rococo-artifacts.sh
+++ scripts/generate-rococo-artifacts.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "##GENERATING RAW CHAINSPEC FOR ROCOCO"
-./target/release/bitgreen-parachain build-spec --chain rococo-local --disable-default-bootnode > ./artifacts/testnet/bitgreen-testnet-raw.json
+./target/release/bitgreen-parachain build-spec --raw --chain local-rococo --disable-default-bootnode > ./artifacts/testnet/bitgreen-testnet-raw.json
 
 echo "##GENERATING GENESIS WASM FOR ROCOCO"
 ./target/release/bitgreen-parachain export-genesis-wasm --chain ./artifacts/testnet/bitgreen-testnet-raw.json > ./artifacts/testnet/bitgreen-testnet-wasm
